c28e506a7d9b68f797d4a3bc0e9cd51c2d25ff00
Boris Sky <bor2002isky@gmail.com>
2022-06-26 20:48:21 +0100

    Added simple join voice channel functionality.

--- src/mainCommands/play.js
+++ src/mainCommands/play.js
@@ -1,3 +1,20 @@
-module.exports = function play(message, parameters) {
-    message.reply('!play not implemented yet!');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+module.exports = async function play(message, parameters) {
+    //message.reply('!play not implemented yet!');
+    const channel = await message.member.voice.channel;
+    if(!channel) {  // Check if user is in a voice channel
+        message.reply('Must join a voice channel first!');
+    } else {
+        if(parameters[0] === undefined) {  // Check if user supplied a parameter to request a search for
+            message.reply('Please supply a valid song request!');
+        } else {
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: message.guild.id,
+                adapterCreator: message.guild.voiceAdapterCreator,
+            });
+            message.channel.send(`Searching **${parameters[0]}**...`);
+        }
+    }
 };
