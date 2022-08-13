79f9de9bbba9e9f84c969bd9158b40440e21dbc5
Boris Sky <bor2002isky@gmail.com>
2022-08-13 21:13:25 +0100

    Removed incorrect comment.

--- src/mainCommands/leave.js
+++ src/mainCommands/leave.js
@@ -1,7 +1,7 @@
 const { getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = async function leave(message, songQueue) {
-    const botInChannel = await getVoiceConnection(message.guild.id);  // message.guild.id acts as the bot's id
+    const botInChannel = await getVoiceConnection(message.guild.id);
     if(!botInChannel) {  // Check if bot is in a voice channel
         message.reply('The bot is not in the voice channel!');
     } else {
