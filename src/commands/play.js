08aeb8c5bbf3811297d4c43900b046fb082d0bcf
Boris Sky <bor2002isky@gmail.com>
2024-02-02 22:19:11 +0000

    Main play functionality abstracted into function, works for video. Need to implement playlist addition to queue and integration

--- src/commands/play.js
+++ src/commands/play.js
@@ -13,52 +13,20 @@ export default async function Play({ message, parameters, songQueue, player }) {
       message.reply("Please supply a valid song request!");
     else {
       const response = await fetchQuery(searchQuery);
-      /* ==========================  Make this a function that both video and playlist can use ==========================
-      const videoURL = `https://www.youtube.com/watch?v=${response.id.videoId}`;
-
-      // This is a workaround for the play-dl package not being able to play age restricted videos
-      let stream = null;
-      try {
-        stream = await play.stream(videoURL, { quality: 2 });
-      } catch (e) {
-        message.reply("This video is age restricted and cannot be played!");
-        return;
-      }
-
-      const connection = joinVoiceChannel({
-        channelId: userInChannel.id,
-        guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
-      });
-      const resource = createAudioResource(stream.stream, {
-        inputType: stream.type,
-      });
-
-      songQueue.push({
-        title: response[0].snippet.title
-          .replaceAll("&#39;", "'")
-          .replaceAll("&amp;", "&")
-          .replaceAll("&quot;", '"'),
-        url: videoURL,
-        resource: resource,
-      });
-
-      if (songQueue.length === 1) {
-        connection.subscribe(player);
-        player.play(songQueue[0].resource);
-      }
-      message.reply(
-        `${videoURL} added to queue at position: **${songQueue.length}**`
-      );
-      */
-      console.log(response.type);
       if (response.data === null || response.data.length === 0) {
         if (response.type === "video")
           message.reply("This video does not exist or is private!");
         else message.reply("This playlist does not exist or is private!");
         return;
       }
-      if (response.type === "video") console.log(response.data[0]);
+      if (response.type === "video")
+        mainPlayFunctionality(
+          message,
+          songQueue,
+          player,
+          userInChannel,
+          response.data[0]
+        );
       else {
         console.log(response.data[0]);
         console.log(response.data[1]);
@@ -77,7 +45,6 @@ async function fetchQuery(query) {
   if (query.includes("https://www.youtube.com/playlist?list=")) {
     const playlistID = query.split("playlist?list=")[1];
     url = `${baseURL}/playlistItems?key=${process.env.YT_API_KEY}&part=snippet&playlistId=${playlistID}&maxResults=50`;
-    // Get more than 50 videos from a playlist
     type = "playlist";
   } else {
     url = `${baseURL}/search?key=${process.env.YT_API_KEY}&type=video&part=snippet&q=${query}`;
@@ -97,4 +64,50 @@ async function fetchQuery(query) {
       data: null,
     };
   }
+}
+
+async function mainPlayFunctionality(
+  message,
+  songQueue,
+  player,
+  userInChannel,
+  response
+) {
+  const videoURL = `https://www.youtube.com/watch?v=${response.id.videoId}`;
+
+  // This is a workaround for the play-dl package not being able to play age restricted videos
+  let stream = null;
+  try {
+    stream = await play.stream(videoURL, { quality: 2 });
+  } catch (e) {
+    message.reply("This video is age restricted and cannot be played!");
+    return;
+  }
+
+  const connection = joinVoiceChannel({
+    channelId: userInChannel.id,
+    guildId: message.guild.id,
+    adapterCreator: message.guild.voiceAdapterCreator,
+  });
+  const resource = createAudioResource(stream.stream, {
+    inputType: stream.type,
+  });
+
+  songQueue.push({
+    title: response.snippet.title
+      .replaceAll("&#39;", "'")
+      .replaceAll("&amp;", "&")
+      .replaceAll("&quot;", '"'),
+    url: videoURL,
+    resource: resource,
+  });
+
+  // Decide if this stays in method or goes outside when experimenting with playlist functionality
+  if (songQueue.length === 1) {
+    connection.subscribe(player);
+    player.play(songQueue[0].resource);
+  }
+  message.reply(
+    `${videoURL} added to queue at position: **${songQueue.length}**`
+  );
 }
