0ee4714370b4f5bb57eb26cdeb30451c2776bce6
Boris Sky <bor2002isky@gmail.com>
2024-02-05 17:48:37 +0000

    Queue now incorporates pagination

--- src/commands/q.js
+++ src/commands/q.js
@@ -1,10 +1,11 @@
 import { MessageEmbed } from "discord.js";
+import { Pagination } from "pagination.djs";
 
 export default function Queue({ message, songQueue }) {
-  if (songQueue.length === 0) message.channel.send("Queue is empty!");
+  if (songQueue.length === 0) message.reply("Queue is empty!");
   else {
     if (songQueue.length === 1) {
-      message.channel.send({
+      message.reply({
         embeds: [
           new MessageEmbed()
             .setColor("#0B1D46")
@@ -17,26 +18,56 @@ export default function Queue({ message, songQueue }) {
         ],
       });
     } else {
-      message.channel.send({
-        embeds: [
-          new MessageEmbed()
-            .setColor("#0B1D46")
-            .setAuthor({ name: `Requested by ${message.author.username}` })
-            .addFields({
-              name: "**Now Playing**",
-              value: `[${songQueue[0].title}](${songQueue[0].url})`,
-            })
-            .addFields({
-              name: "**Queue**",
-              value: `${songQueue
-                .slice(1)
-                .map(
-                  (item, index) => `${index + 2}. [${item.title}](${item.url})`
-                )
-                .join("\n")}`,
-            }),
-        ],
+      const pagination = new Pagination(message, {
+        idle: 60000,
       });
+      const itemsPerPage = 7;
+      const firstSong = songQueue[0];
+      const remainderSongQueue = songQueue.slice(1);
+      // -1 to account for the first song
+      const embeds = embedGenerator(
+        message,
+        firstSong,
+        remainderSongQueue,
+        itemsPerPage - 1
+      );
+
+      pagination.setEmbeds(embeds, (embed, index, array) => {
+        return embed.setFooter({
+          text: `Page: ${index + 1}/${array.length} \tQueue Size: ${
+            songQueue.length
+          }`,
+        });
+      });
+      pagination.render();
     }
   }
+}
+
+function embedGenerator(message, firstSong, songQueue, itemsPerPage) {
+  const embeds = [];
+  let counter = 0;
+  const numberOfPages = Math.ceil(songQueue.length / itemsPerPage);
+  for (let i = 0; i < numberOfPages; i++) {
+    const newEmbed = new MessageEmbed()
+      .setColor("#0B1D46")
+      .setAuthor({ name: `Requested by ${message.author.username}` })
+      .addFields({
+        name: "**Now Playing**",
+        value: `[${firstSong.title}](${firstSong.url})`,
+      })
+      .addFields({
+        name: "**Queue**",
+        value: `${songQueue
+          .slice(counter, counter + itemsPerPage)
+          .map(
+            (item, index) =>
+              `${index + 2 + counter}. [${item.title}](${item.url})`
+          )
+          .join("\n")}`,
+      });
+    embeds.push(newEmbed);
+    counter += itemsPerPage;
+  }
+  return embeds;
 }
