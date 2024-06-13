17f63c05ea94b71026c21d98044106758a8d7911
Boris Sky <bor2002isky@gmail.com>
2024-06-13 13:11:41 +0100

    Increased timer to delete message sent by bot after bot leaves vc and added emoji to message

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -4,8 +4,8 @@ export default async function Leave({ message, songQueue }) {
   const botInChannel = getVoiceConnection(message.guild.id);
   if (!botInChannel) message.reply("The bot is not in the voice channel!");
   else {
-    await message.channel.send("Left the voice channel!").then((msg) => {
-      setTimeout(() => msg.delete(), 500);
+    await message.channel.send("Left the voice channel :wave:").then((msg) => {
+      setTimeout(() => msg.delete(), 3000);
     });
     botInChannel.destroy();
     songQueue.splice(0, songQueue.length); // Empty the queue
