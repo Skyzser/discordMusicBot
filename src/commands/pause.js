ccfc068a5891e749aa950ac8680c7bb2b6d824ec
Boris Sky <bor2002isky@gmail.com>
2024-06-13 13:12:20 +0100

    Added new emoji and increased timer to delete message sent by bot

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -3,13 +3,13 @@ import { AudioPlayerStatus } from "@discordjs/voice";
 export default async function Pause({ player, message }) {
   if (player.state.status === AudioPlayerStatus.Paused) {
     player.unpause();
-    await message.channel.send("Playing :play_pause:").then((msg) => {
-      setTimeout(() => msg.delete(), 1000);
+    await message.channel.send("Playing :arrow_forward:").then((msg) => {
+      setTimeout(() => msg.delete(), 3000);
     });
   } else {
     player.pause();
-    await message.channel.send("Paused :play_pause:").then((msg) => {
-      setTimeout(() => msg.delete(), 1000);
+    await message.channel.send("Paused :pause_button:").then((msg) => {
+      setTimeout(() => msg.delete(), 3000);
     });
   }
 }
