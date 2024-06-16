30173e2a518a63e62b6c6477e5d6455bbd5bd9ee
Boris Sky <bor2002isky@gmail.com>
2024-06-16 23:23:14 +0100

    Removed leave message

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -4,9 +4,6 @@ export default async function Leave({ message, songQueue }) {
   const botInChannel = getVoiceConnection(message.guild.id);
   if (!botInChannel) message.reply("The bot is not in the voice channel!");
   else {
-    await message.channel.send("Left the voice channel :wave:").then((msg) => {
-      setTimeout(() => msg.delete(), 500);
-    });
     botInChannel.destroy();
     songQueue.splice(0, songQueue.length); // Empty the queue
   }
